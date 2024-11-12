ec15e4fccab97cda2dad7493dc838d30d16369de
Luís Cipriani <cunhaluisg5@gmail.com>
2024-11-11 21:37:32 -0300

    Cria menu de destaque

---
+++ src/components/destaque/Destaque.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-carousel
+      v-model="indiceAtual"
+      cycle
+      interval="5000"
+      show-arrows
+      hide-delimiters
+    >
+      <v-carousel-item
+        v-for="(imagem, indice) in imagens"
+        :key="indice"
+      >
+        <v-img :src="imagem" class="d-flex justify-center align-center" />
+      </v-carousel-item>
+    </v-carousel>
+</template>
+
+<script>
+  import imagem01 from "../../assets/01.png";
+  import imagem02 from "../../assets/02.png";
+  import imagem03 from "../../assets/03.png";
+
+  export default {
+    name: "Destaque",
+    data() {
+    return {
+      indiceAtual: 0,
+      imagens: [
+        imagem01,
+        imagem02,
+        imagem03,
+      ],
+    };
+  },
+  }
+</script>
+
+<style scoped>
+  @import './Destaque.css';
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,11 @@
 <template>
   <Cabecalho />
   <Menu />
+  <Destaque />
 </template>
 
 <script setup>
   import Cabecalho from "../components/Cabecalho/Cabecalho.vue";
   import Menu from "../components/menu/Menu.vue";
+  import Destaque from '../components/destaque/Destaque.vue';
 </script>
